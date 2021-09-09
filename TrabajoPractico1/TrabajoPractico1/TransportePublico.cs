b51572a957dd186461b2e5c805c91eb758bfc0c3
Mamani Mario <mario_89_7@hotmail.com>
2021-09-09 08:05:07 -0300

    Creando la Clase abstracta TransportePublico

---
+++ TrabajoPractico1/TrabajoPractico1/TransportePublico.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico1
+{
+    public abstract class TransportePublico
+    {
+        private int pasajeros;
+        protected TransportePublico(int pasajeros)
+        {
+            this.pasajeros = pasajeros;
+        }
+
+        public int Pasajeros
+        {
+            get { return pasajeros; }
+            set
+            {
+                pasajeros = value;
+                if (pasajeros < 0)
+                {
+                    throw new Exception("no se puede ingresar numeros negativos");
+                }
+
+            }
+        }
+
+        public abstract void avanzar();
+        public abstract void detenerse();
+    }
+}
