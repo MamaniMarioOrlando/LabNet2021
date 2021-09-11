2f2c3dba63002d26117b4bd72db6c5ec5e4dd30c
Mamani Mario <mario_89_7@hotmail.com>
2021-09-11 00:41:19 -0300

    Resolucion del ejercicio uno
    Aclaro que cuando capturo la Exception en el metodo Dividir muestra el mensaje de la excepcion y no un mensaje personalizado.

---
+++ TrabajoPractico2/TrabajoPractico2/ExcepcionDeDivision.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TrabajoPractico2
+{
+    public class ExcepcionDeDivision
+    {
+        public static void PedirUnValor() 
+        {
+            Console.WriteLine("***Ejercicio 1 capturando Exepcion***\n");
+            
+            Console.Write("Ingrese un numero: ");
+            try
+            {
+                int numero = Convert.ToInt32(Console.ReadLine());
+                Division(numero);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("\n Message: {0}", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nLa operacion a finalizado!");
+            }
+
+        }
+        private static void Division(int valor)
+        {
+            int resultado;
+
+            try
+            {
+                resultado = valor /0;
+                Console.WriteLine("El resultado de la divisio es: {0}", resultado);
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                Console.WriteLine("\n Message: {0}", ex.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("\nLa operacion a finalizado!");
+            }
+        }
+    }
+}
