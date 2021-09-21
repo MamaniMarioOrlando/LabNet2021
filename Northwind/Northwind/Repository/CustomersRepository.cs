211d7a862df50be04851dfefc0683db5fe070f03
Mamani Mario <mario_89_7@hotmail.com>
2021-09-21 18:18:28 -0300

    entrega de tp4

---
+++ Northwind/Northwind/Repository/CustomersRepository.cs
@@ -0,0 +1,13 @@
+﻿using AquariumService.Abstractions;
+using NorthwindService.Models;
+
+namespace NorthwindService.Repository
+{
+    public class CustomersRepository : GenericRepository<Customer>
+    {
+        public CustomersRepository(NorthwindContext context) : base(context)
+        {
+
+        }
+    }
+}
